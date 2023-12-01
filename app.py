28ad60b74b346d2d5d1fb1cbe615c64c4b951883
vishymails <vishymails@gmail.com>
2023-12-01 12:09:26 +0530

    prediction validation added

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 import os
-
+from prediction_service import prediction
 import numpy as np
 
 
@@ -18,16 +18,16 @@ def index() :
     if request.method == "POST" :
         try :
             if request.form :
-                data = dict(request.form).values()
-                data = [list(map(float, data))]
-                response = predict(data)
+                dict_req = dict(request.form)
+                response = prediction.form_response(dict_req)
                 return render_template("index.html", response=response)
             elif request.json :
-                response = api_response(request)
+                response = prediction.api_response(request.json)
                 return jsonify(response)
         except Exception as e :
             print(e)
             error = {"error" : "Something went wrong !! Try again "}
+            error = {"error" : e}
             return render_template("404.html", error=error)
     else :
         return render_template("index.html")
--- prediction_service/prediction.py
+++ prediction_service/prediction.py
@@ -38,17 +38,55 @@ def predict(data) :
         return "Unexpected result"
 
 
+def get_schema(schema_path=schema_path) :
+    with open(schema_path) as json_file :
+        schema = json.load(json_file)
+    return schema
 
 
+def validate_input(dict_request) :
+    def _validate_cols(col) :
+        schema = get_schema()
+        actual_cols = schema.keys()
+        if col not in actual_cols :
+            raise NotInCols
+
+    def _validate_values(col, val) :
+        schema = get_schema()
+        if not(schema[col]["min"] <= float(dict_request[col]) <= schema[col]["max"]) :
+            raise NotInRange
+
+    for col, val in dict_request.items() :
+        _validate_cols(col)
+        _validate_values(col, val)
+
+    return True
 
 
-
-def api_response(request) :
-    try :
-        data = np.array([list(request.json.values())])
+def form_response(dict_request) :
+    if validate_input(dict_request) :
+        data = dict_request.values()
+        data = [list(map(float, data))]
         response = predict(data)
-        response = {"response":response}
-        return response 
+        return response
+
+
+
+
+
+def api_response(dict_request) :
+    try :
+        if validate_input(dict_request) :
+            data = np.array([list(dict_request.values())])
+            response = predict(data)
+            response = {"response":response}
+            return response 
+    except NotInRange as e :
+            response = {"the_expected_range" : get_schema(), "response" : str(e)}
+            print(response)
+    except NotInCols as e :
+            response = {"the_expected_cols" : get_schema().keys(), "response" : str(e)}
+            print(response)
     except Exception as e :
             print(e)
             error = {"error" : "Something went wrong !! Try again "}
